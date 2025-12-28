7d557af74b8acf5367fe403b34471150a2be082a
Obinnachieze <wisdomchieze0@gmail.com>
2025-12-28 09:39:09 +0100

    new

--- wisdom.js
+++ wisdom.js
@@ -1,21 +1,48 @@
-let firstCard = 10;
-let secondCard = 11;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 
 const messageEl = document.getElementById("message-1");
 const sum1 = document.getElementById("sum");
 const cardEl = document.getElementById("cards");
+const player = {
+  name: "Player",
+  chips: 145,
+};
+const playerEl = document.getElementById("per");
+playerEl.textContent = player.name + ": $" + player.chips;
+
+function Random() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber === 1) {
+    return 11;
+  } else if (randomNumber > 10) {
+    return 10;
+  } else {
+    return randomNumber;
+  }
+}
+
+function getRandomCard() {
+  return Random();
+}
 
 const startGame = () => {
+  isAlive = true;
+  const firstCard = getRandomCard();
+  const secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   heyGame();
 };
 
 const heyGame = () => {
-  cardEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+  cardEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardEl.textContent += cards[i] + " ";
+  }
   sum1.textContent = "Sum: " + sum;
   if (sum < 21) {
     message = "Do you want to draw a new card?";
@@ -31,9 +58,11 @@ const heyGame = () => {
 };
 
 const drawNewCard = () => {
-  const newCard = 6;
-  sum += newCard;
-  cards.push(newCard);
-  heyGame();
+  if (isAlive === true && hasBlackJack === false) {
+    const newCard = getRandomCard();
+    sum += newCard;
+    cards.push(newCard);
+    heyGame();
+  }
 };
 console.log(cards);
